109cb3c8feeaa3a88e248aaeae8064506bc100b8
padmec-sm <sidicleyribeiro@gmail.com>
2018-03-22 20:23:55 -0300

    Refactoring InterpolMethod with mesh data inheritance

--- pressure_solver_2D.py
+++ pressure_solver_2D.py
@@ -42,7 +42,6 @@ class MpfaD2D:
         self.A = np.zeros([len(self.all_volumes), len(self.all_volumes)])
         self.B = np.zeros([len(self.all_volumes), 1])
 
-
     def get_vect_module(self, vector):
         vector = np.array(vector)
         dot_product = np.dot(vector, vector)
@@ -438,8 +437,8 @@ class MpfaD2D:
                         self.B[v_ids[well_volume]][0] = well_pressure
             # print("Calculou face ", count, " de ", len(self.all_faces))
 
-        # print(self.A)
-        # print(self.B)
+        print(self.A)
+        print(self.B)
         volume_pressures = np.linalg.solve(self.A, self.B)
         # print(volume_pressures)
         self.mb.tag_set_data(self.pressure_tag, self.all_volumes, volume_pressures.flatten())
@@ -518,10 +517,10 @@ class MpfaD2D:
     #     return face_grad
 
 
-class InterpolMethod:
+class InterpolMethod(MpfaD2D):
 
-    def __init__(self, mpfad, dist_factor=0.5):
-        self.mpfad = mpfad
+    def __init__(self, mesh_data, dist_factor=0.5):
+        self.mesh_data = mesh_data
         self.dist_factor = dist_factor
 
     def _flux_term(self, vector_1st, permeab, vector_2nd, face_area):
@@ -531,40 +530,60 @@ class InterpolMethod:
         return flux_term
 
     def _get_face_weight(self, interp_node, face):
-        crds_node = self.mpfad.mb.get_coords([interp_node])
-        adjacent_volumes = self.mpfad.mb.get_adjacencies(face, 2)
-        half_face = self.mpfad.mtu.get_average_position([face])
+        crds_node = self.mesh_data.mb.get_coords([interp_node])
+        adjacent_volumes = self.mesh_data.mb.get_adjacencies(face, 2)
+        half_face = self.mesh_data.mtu.get_average_position([face])
         csi_num = 0
         csi_den = 0
         for adjacent_volume in adjacent_volumes:
-            centroid_volume = self.mpfad.mesh_data.get_centroid(adjacent_volume)
-            perm_volume = self.mpfad.mesh_data.mb.tag_get_data(self.mpfad.perm_tag, adjacent_volume).reshape([3, 3])
+            centroid_volume = self.mesh_data.get_centroid(adjacent_volume)
+            perm_volume = self.mesh_data.mb.tag_get_data(
+                self.mesh_data.perm_tag, adjacent_volume).reshape([3, 3])
 
-            interp_node_adj_faces = set(self.mpfad.mtu.get_bridge_adjacencies(interp_node, 0, 1))
-            adjacent_volume_adj_faces = set(self.mpfad.mtu.get_bridge_adjacencies(adjacent_volume, 1, 1))
+            interp_node_adj_faces = set(
+                self.mesh_data.mtu.get_bridge_adjacencies(interp_node, 0, 1))
+            adjacent_volume_adj_faces = set(
+                self.mesh_data.mtu.get_bridge_adjacencies(adjacent_volume, 1, 1))
 
-            faces = np.asarray(list(interp_node_adj_faces & adjacent_volume_adj_faces), dtype='uint64')
+            faces = np.asarray(list(
+                interp_node_adj_faces & adjacent_volume_adj_faces), dtype='uint64')
             other_face = faces[faces != face]
-            half_other_face = self.mpfad.mtu.get_average_position(other_face)
+            half_other_face = self.mesh_data.mtu.get_average_position(other_face)
 
-            K_bar_n = self.mpfad._get_conormal_prod(np.asarray([half_other_face, half_face]), centroid_volume, perm_volume)
+            K_bar_n = self._get_conormal_prod(
+                np.asarray([half_other_face, half_face]),
+                centroid_volume, perm_volume)
 
-            aux_dot_num = np.dot(crds_node - half_other_face, half_face - half_other_face)
-            cot_num = aux_dot_num / (2.0 * self.mpfad.trian_area(half_face, half_other_face, crds_node))
+            aux_dot_num = np.dot(
+                crds_node - half_other_face, half_face - half_other_face)
+            cot_num = aux_dot_num / (2.0 * self.trian_area(
+                half_face, half_other_face, crds_node))
             # print("cot_num: ", cot_num, crds_node, half_face, self.mesh_data.get_centroid(adjacent_volume))
             vector_pseudo_face = half_face - half_other_face
-            normal_pseudo_face = self.mpfad.area_vector(half_face, half_other_face, crds_node)
-            module_squared_vector = np.dot(vector_pseudo_face, vector_pseudo_face)
-            K_bar_t = np.dot(np.dot(normal_pseudo_face, perm_volume), vector_pseudo_face)/module_squared_vector
+            normal_pseudo_face = self.area_vector(half_face,
+                                                        half_other_face,
+                                                        crds_node)
+            module_squared_vector = np.dot(vector_pseudo_face,
+                                           vector_pseudo_face)
+            K_bar_t = np.dot(np.dot(normal_pseudo_face, perm_volume),
+                vector_pseudo_face)/module_squared_vector
 
             csi_num += K_bar_n * cot_num + K_bar_t
             # print("")
-            K_den_n = self.mpfad._get_conormal_prod(np.asarray([crds_node, half_face]), centroid_volume, perm_volume)
+            K_den_n = self._get_conormal_prod(
+                np.asarray([crds_node, half_face]),
+                centroid_volume, perm_volume)
 
-            aux_dot_den = np.dot(half_face - crds_node, centroid_volume - crds_node)
-            cot_den = aux_dot_den / (2.0 * self.mpfad.trian_area(half_face, centroid_volume, crds_node))
+            aux_dot_den = np.dot(half_face - crds_node,
+                                 centroid_volume - crds_node)
+            cot_den = aux_dot_den / (
+                2.0 * self.trian_area(half_face,
+                                            centroid_volume,
+                                            crds_node))
 
-            K_den_t = self.mpfad.K_t_X(np.asarray([crds_node, half_face]), centroid_volume, perm_volume)
+            K_den_t = self.K_t_X(np.asarray([crds_node, half_face]),
+                                                  centroid_volume,
+                                                  perm_volume)
 
             csi_den += K_den_n * cot_den + K_den_t
 
@@ -575,31 +594,33 @@ class InterpolMethod:
     def _get_neta(self, face, intern_node, cent_node, cent_adj_vol, vol_perm):
         half_face_vect = cent_node - intern_node
         half_face = sqrt(np.dot(half_face_vect, half_face_vect))
-        face_nodes = self.mpfad.mb.get_adjacencies(face, 0)
-        face_nodes_crds = self.mpfad.mb.get_coords(face_nodes).reshape([2, 3])
-        height = self.mpfad.get_face_dist(face_nodes_crds, cent_adj_vol, vol_perm)
+        face_nodes = self.mesh_data.mb.get_adjacencies(face, 0)
+        face_nodes_crds = self.mesh_data.mb.get_coords(face_nodes).reshape([2, 3])
+        height = self.get_face_dist(face_nodes_crds,
+                                          cent_adj_vol,
+                                          vol_perm)
         neta = half_face / height
         return neta
 
     def _get_dynamic_point(self, crds_node, node, face, dist_factor):
-        face_nodes = self.mpfad.mb.get_adjacencies(face, 0)
+        face_nodes = self.mesh_data.mb.get_adjacencies(face, 0)
         face_nodes = np.asarray(face_nodes, dtype='uint64')
         other_node = face_nodes[face_nodes != node]
-        other_crds = self.mpfad.mb.get_coords(other_node)
+        other_crds = self.mesh_data.mb.get_coords(other_node)
         tan_vector = other_crds - crds_node
         dynamic_point = crds_node + dist_factor * tan_vector
         return dynamic_point
 
     def _get_volume_weight(self, node, adjacent_volume, dist_factor):
-        crds_node = self.mpfad.mb.get_coords([node])
-        perm_adjacent_volume = self.mpfad.mb.tag_get_data(self.mpfad.perm_tag, adjacent_volume).reshape([3, 3])
-        cent_adj_vol = self.mpfad.mesh_data.get_centroid(adjacent_volume)
+        crds_node = self.mesh_data.mb.get_coords([node])
+        perm_adjacent_volume = self.mesh_data.mb.tag_get_data(
+            self.mesh_data.perm_tag, adjacent_volume).reshape([3, 3])
+        cent_adj_vol = self.mesh_data.get_centroid(adjacent_volume)
 
-        adjacent_faces = list(set(self.mpfad.mtu.get_bridge_adjacencies(node, 0, 1)) &
-                              set(self.mpfad.mtu.get_bridge_adjacencies(adjacent_volume, 1, 1)))
+        adjacent_faces = list(set(self.mesh_data.mtu.get_bridge_adjacencies(node, 0, 1)) &
+                              set(self.mesh_data.mtu.get_bridge_adjacencies(adjacent_volume, 1, 1)))
         first_face = adjacent_faces[0]
         second_face = adjacent_faces[1]
-
         dyn_point_1st = self._get_dynamic_point(crds_node,
                                                 node,
                                                 first_face,
@@ -608,11 +629,14 @@ class InterpolMethod:
                                                 node,
                                                 second_face,
                                                 dist_factor)
-
-        K_ni_first = self.mpfad._get_conormal_prod(np.asarray([crds_node, dyn_point_1st]),
-                           cent_adj_vol, perm_adjacent_volume)
-        K_ni_second = self.mpfad._get_conormal_prod(np.asarray([crds_node, dyn_point_2nd]),
-                            cent_adj_vol, perm_adjacent_volume)
+        K_ni_first = self._get_conormal_prod(np.asarray([crds_node,
+                                                   dyn_point_1st]),
+                                                   cent_adj_vol,
+                                                   perm_adjacent_volume)
+        K_ni_second = self._get_conormal_prod(np.asarray([crds_node,
+                                                    dyn_point_2nd]),
+                                                    cent_adj_vol,
+                                                    perm_adjacent_volume)
 
         neta_1st = self._get_neta(first_face, crds_node, dyn_point_1st,
                                   cent_adj_vol, perm_adjacent_volume)
@@ -622,14 +646,15 @@ class InterpolMethod:
         csi_first = self._get_face_weight(node, first_face)
         csi_second = self._get_face_weight(node, second_face)
 
-        node_weight = K_ni_first * neta_1st * csi_first + K_ni_second * neta_2nd * csi_second
+        node_weight = K_ni_first * neta_1st * csi_first + \
+                      K_ni_second * neta_2nd * csi_second
         # print("weight: ", node_weight, crds_node, cent_adj_vol)
         return node_weight
 
     def by_lpew2(self, node):
         # node = np.asarray([node], dtype='uint64')
-        # print("NODE", node, self.mpfad.mesh_data.mb.get_coords([node]))
-        vols_around = self.mpfad.mb.get_adjacencies([node], 2)
+        # print("NODE", node, self.mesh_data.mb.get_coords([node]))
+        vols_around = self.mesh_data.mb.get_adjacencies([node], 2)
         weight_sum = 0.0
         weights = np.array([])
         for a_volume in vols_around:
--- tests/test_problems.py
+++ tests/test_problems.py
@@ -24,7 +24,7 @@ class PressureSolverTest(unittest.TestCase):
         self.mesh_1.set_boundary_condition('Neumann', {201: 0.0},
                                            dim_target=1, set_nodes=True)
         self.mpfad_1 = MpfaD2D(self.mesh_1)
-        self.imd_1 = InterpolMethod(self.mpfad_1)
+        self.imd_1 = InterpolMethod(self.mesh_1)
 
         self.mesh_2 = MeshManager('mesh_test_2.msh', dim=2)
         self.mesh_2.set_media_property('Permeability', {1: K_1}, dim_target=2)
@@ -33,7 +33,7 @@ class PressureSolverTest(unittest.TestCase):
         self.mesh_2.set_boundary_condition('Neumann', {201: 0.0},
                                            dim_target=1, set_nodes=True)
         self.mpfad_2 = MpfaD2D(self.mesh_2)
-        self.imd_2 = InterpolMethod(self.mpfad_2)
+        self.imd_2 = InterpolMethod(self.mesh_2)
 
         self.mesh_3 = MeshManager('mesh_test_struct_layers.msh', dim=2)
         self.mesh_3.set_media_property('Permeability', {1: K_2, 2: K_1},
@@ -56,10 +56,10 @@ class PressureSolverTest(unittest.TestCase):
         # mb = nmg.crazy_mesh()
         # self.mesh_xconf.load_data(mb)
         # self.mpfad_xconf = MpfaD2D(self.mesh_xconf)
-        # self.imd_xconf = InterpolMethod(self.mpfad_xconf)
+        # self.imd_xconf = InterpolMethod(self.mesh_xconf)
 
     def test_linear_prob_with_struct_mesh_and_dyn_point_at_two_thirds(self):
-        imd = InterpolMethod(self.mpfad_1, 2.0/3.0)
+        imd = InterpolMethod(self.mesh_1, 2.0/3.0)
         self.mpfad_1.run_solver(imd.by_lpew2)
         for a_volume in self.mesh_1.all_volumes:
             local_pressure = self.mesh_1.mb.tag_get_data(
@@ -69,7 +69,7 @@ class PressureSolverTest(unittest.TestCase):
                 local_pressure[0][0], 1 - coord_x, delta=1e-15)
 
     def test_linear_prob_with_unstruct_mesh_and_dyn_point_at_two_thirds(self):
-        imd = InterpolMethod(self.mpfad_2, 2.0/3.0)
+        imd = InterpolMethod(self.mesh_2, 2.0/3.0)
         self.mpfad_2.run_solver(imd.by_lpew2)
         for a_volume in self.mesh_2.all_volumes:
             local_pressure = self.mesh_2.mb.tag_get_data(
@@ -79,7 +79,7 @@ class PressureSolverTest(unittest.TestCase):
                 local_pressure[0][0], 1 - coord_x, delta=1e-15)
 
     def test_linear_prob_with_struct_mesh_and_dyn_point_at_adj_node(self):
-        imd = InterpolMethod(self.mpfad_1, 1.0)
+        imd = InterpolMethod(self.mesh_1, 1.0)
         self.mpfad_1.run_solver(imd.by_lpew2)
         for a_volume in self.mesh_1.all_volumes:
             local_pressure = self.mesh_1.mb.tag_get_data(
@@ -89,7 +89,7 @@ class PressureSolverTest(unittest.TestCase):
                 local_pressure[0][0], 1 - coord_x, delta=1e-15)
 
     def test_linear_prob_with_unstruct_mesh_and_dyn_point_at_adj_node(self):
-        imd = InterpolMethod(self.mpfad_2, 1.0)
+        imd = InterpolMethod(self.mesh_2, 1.0)
         self.mpfad_2.run_solver(imd.by_lpew2)
         for a_volume in self.mesh_2.all_volumes:
             local_pressure = self.mesh_2.mb.tag_get_data(
@@ -120,7 +120,7 @@ class PressureSolverTest(unittest.TestCase):
                 self.assertAlmostEqual(weight, 0.25, delta=1e-12)
 
     def test_piece_wise_lin_prob_struct_with_dyn_point_at_two_thirds(self):
-        imd = InterpolMethod(self.mpfad_3, 2/3.0)
+        imd = InterpolMethod(self.mesh_3, 2/3.0)
         vols_press = self.mpfad_3.run_solver(imd.by_lpew2)
         all_volumes = self.mesh_3.all_volumes
         for a_volume, vol_pres in zip(all_volumes, vols_press):
@@ -133,7 +133,7 @@ class PressureSolverTest(unittest.TestCase):
                     vol_pres[0], (4/3.0)*(1 - coord_x), delta=1e-15)
 
     def test_piece_wise_lin_prob_struct_with_dyn_point_at_adj_point(self):
-        imd = InterpolMethod(self.mpfad_3, 1.0)
+        imd = InterpolMethod(self.mesh_3, 1.0)
         vols_press = self.mpfad_3.run_solver(imd.by_lpew2)
         all_volumes = self.mesh_3.all_volumes
         for a_volume, vol_pres in zip(all_volumes, vols_press):
@@ -146,7 +146,7 @@ class PressureSolverTest(unittest.TestCase):
                     vol_pres[0], (4/3.0)*(1 - coord_x), delta=1e-15)
 
     def test_piece_wise_lin_prob_unstruct_with_dyn_point_at_two_thirds(self):
-        imd = InterpolMethod(self.mpfad_4, 2/3.0)
+        imd = InterpolMethod(self.mesh_4, 2/3.0)
         vols_press = self.mpfad_4.run_solver(imd.by_lpew2)
         all_volumes = self.mesh_4.all_volumes
         for a_volume, vol_pres in zip(all_volumes, vols_press):
@@ -159,7 +159,7 @@ class PressureSolverTest(unittest.TestCase):
                     vol_pres[0], (4/3.0)*(1 - coord_x), delta=1e-14)
 
     def test_piece_wise_lin_prob_unstruct_with_dyn_point_at_adj_point(self):
-        imd = InterpolMethod(self.mpfad_4, 1.0)
+        imd = InterpolMethod(self.mesh_4, 1.0)
         vols_press = self.mpfad_4.run_solver(imd.by_lpew2)
         all_volumes = self.mesh_4.all_volumes
         for a_volume, vol_pres in zip(all_volumes, vols_press):
