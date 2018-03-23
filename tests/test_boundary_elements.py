655af8d569f42a7be2a795ec567e81541ed77348
padmec-sm <sidicleyribeiro@gmail.com>
2018-03-23 15:02:55 -0300

    Commenting some temporarily unused funcionality

--- tests/test_boundary_elements.py
+++ tests/test_boundary_elements.py
@@ -2,7 +2,7 @@ import unittest
 import numpy as np
 from pressure_solver_2D import MpfaD2D
 from mesh_preprocessor import MeshManager
-import nonconform_mesh_generator_test as nmg
+# import nonconform_mesh_generator_test as nmg
 
 class PressureSolverTest(unittest.TestCase):
 
@@ -24,10 +24,10 @@ class PressureSolverTest(unittest.TestCase):
                                            dim_target=1, set_nodes=True)
         self.mpfad_1 = MpfaD2D(self.mesh_1)
 
-        self.mesh_2 = MeshManager('mesh_nonconform_test.vtk', dim=2)
-        mb = nmg.crazy_mesh()
-        self.mesh_2.load_data(mb)
-        self.mpfad_2 = MpfaD2D(self.mesh_2)
+        # self.mesh_2 = MeshManager('mesh_nonconform_test.vtk', dim=2)
+        # mb = nmg.crazy_mesh()
+        # self.mesh_2.load_data(mb)
+        # self.mpfad_2 = MpfaD2D(self.mesh_2)
 
     def test_if_method_has_all_dirichlet_nodes(self):
         self.assertEqual(len(self.mpfad_1.dirichlet_nodes), 8)
@@ -50,14 +50,14 @@ class PressureSolverTest(unittest.TestCase):
     def test_if_method_has_all_intern_faces(self):
         self.assertEqual(len(self.mpfad_1.intern_faces), 12)
 
-    def test_load_data_neumann_faces_from_vtk_with_usable_data_on_it(self):
-        self.assertEqual(len(self.mpfad_2.neumann_faces), 28)
-
-    def test_load_data_dirichlet_faces_from_vtk_with_usable_data_on_it(self):
-        self.assertEqual(len(self.mpfad_2.dirichlet_faces), 28)
-
-    def test_load_data_dirichlet_nodes_from_vtk_with_usable_data_on_it(self):
-        self.assertEqual(len(self.mpfad_2.dirichlet_nodes), 30)
-
-    def test_load_data_neumann_nodes_from_vtk_with_usable_data_on_it(self):
-        self.assertEqual(len(self.mpfad_2.neumann_nodes), 26)
+    # def test_load_data_neumann_faces_from_vtk_with_usable_data_on_it(self):
+    #     self.assertEqual(len(self.mpfad_2.neumann_faces), 28)
+    #
+    # def test_load_data_dirichlet_faces_from_vtk_with_usable_data_on_it(self):
+    #     self.assertEqual(len(self.mpfad_2.dirichlet_faces), 28)
+    #
+    # def test_load_data_dirichlet_nodes_from_vtk_with_usable_data_on_it(self):
+    #     self.assertEqual(len(self.mpfad_2.dirichlet_nodes), 30)
+    #
+    # def test_load_data_neumann_nodes_from_vtk_with_usable_data_on_it(self):
+    #     self.assertEqual(len(self.mpfad_2.neumann_nodes), 26)
