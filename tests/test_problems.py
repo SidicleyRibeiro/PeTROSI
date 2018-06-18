49f82d84a2152798709d5f6cb2e7adc665f96b83
Sidicley <sidicleyribeiro@gmail.com>
2018-06-18 07:02:50 -0300

    Altering InterpolMethods import

--- tests/test_problems.py
+++ tests/test_problems.py
@@ -2,8 +2,9 @@ import unittest
 import numpy as np
 from pressure_solver_2D import MpfaD2D
 from mesh_preprocessor import MeshManager
-from pressure_solver_2D import InterpolMethod
+from interpolation_methods import InterpolMethods
 # import nonconform_mesh_generator_test as nmg
+
 
 class PressureSolverTest(unittest.TestCase):
 
@@ -24,7 +25,7 @@ class PressureSolverTest(unittest.TestCase):
         self.mesh_1.set_boundary_condition('Neumann', {201: 0.0},
                                            dim_target=1, set_nodes=True)
         self.mpfad_1 = MpfaD2D(self.mesh_1)
-        self.imd_1 = InterpolMethod(self.mesh_1)
+        self.imd_1 = InterpolMethods(self.mesh_1)
 
         self.mesh_2 = MeshManager('mesh_test_2.msh', dim=2)
         self.mesh_2.set_media_property('Permeability', {1: K_1}, dim_target=2)
@@ -33,7 +34,7 @@ class PressureSolverTest(unittest.TestCase):
         self.mesh_2.set_boundary_condition('Neumann', {201: 0.0},
                                            dim_target=1, set_nodes=True)
         self.mpfad_2 = MpfaD2D(self.mesh_2)
-        self.imd_2 = InterpolMethod(self.mesh_2)
+        self.imd_2 = InterpolMethods(self.mesh_2)
 
         self.mesh_3 = MeshManager('mesh_test_struct_layers.msh', dim=2)
         self.mesh_3.set_media_property('Permeability', {1: K_2, 2: K_1},
@@ -56,10 +57,10 @@ class PressureSolverTest(unittest.TestCase):
         # mb = nmg.crazy_mesh()
         # self.mesh_xconf.load_data(mb)
         # self.mpfad_xconf = MpfaD2D(self.mesh_xconf)
-        # self.imd_xconf = InterpolMethod(self.mesh_xconf)
+        # self.imd_xconf = InterpolMethods(self.mesh_xconf)
 
     def test_linear_prob_with_struct_mesh_and_dyn_point_at_two_thirds(self):
-        imd = InterpolMethod(self.mesh_1, 2.0/3.0)
+        imd = InterpolMethods(self.mesh_1, 2.0/3.0)
         self.mpfad_1.run_solver(imd.by_lpew2)
         for a_volume in self.mesh_1.all_volumes:
             local_pressure = self.mesh_1.mb.tag_get_data(
@@ -69,7 +70,7 @@ class PressureSolverTest(unittest.TestCase):
                 local_pressure[0][0], 1 - coord_x, delta=1e-15)
 
     def test_linear_prob_with_unstruct_mesh_and_dyn_point_at_two_thirds(self):
-        imd = InterpolMethod(self.mesh_2, 2.0/3.0)
+        imd = InterpolMethods(self.mesh_2, 2.0/3.0)
         self.mpfad_2.run_solver(imd.by_lpew2)
         for a_volume in self.mesh_2.all_volumes:
             local_pressure = self.mesh_2.mb.tag_get_data(
@@ -79,7 +80,7 @@ class PressureSolverTest(unittest.TestCase):
                 local_pressure[0][0], 1 - coord_x, delta=1e-15)
 
     def test_linear_prob_with_struct_mesh_and_dyn_point_at_adj_node(self):
-        imd = InterpolMethod(self.mesh_1, 1.0)
+        imd = InterpolMethods(self.mesh_1, 1.0)
         self.mpfad_1.run_solver(imd.by_lpew2)
         for a_volume in self.mesh_1.all_volumes:
             local_pressure = self.mesh_1.mb.tag_get_data(
@@ -89,7 +90,7 @@ class PressureSolverTest(unittest.TestCase):
                 local_pressure[0][0], 1 - coord_x, delta=1e-15)
 
     def test_linear_prob_with_unstruct_mesh_and_dyn_point_at_adj_node(self):
-        imd = InterpolMethod(self.mesh_2, 1.0)
+        imd = InterpolMethods(self.mesh_2, 1.0)
         self.mpfad_2.run_solver(imd.by_lpew2)
         for a_volume in self.mesh_2.all_volumes:
             local_pressure = self.mesh_2.mb.tag_get_data(
@@ -120,7 +121,7 @@ class PressureSolverTest(unittest.TestCase):
                 self.assertAlmostEqual(weight, 0.25, delta=1e-12)
 
     def test_piece_wise_lin_prob_struct_with_dyn_point_at_two_thirds(self):
-        imd = InterpolMethod(self.mesh_3, 2/3.0)
+        imd = InterpolMethods(self.mesh_3, 2/3.0)
         vols_press = self.mpfad_3.run_solver(imd.by_lpew2)
         all_volumes = self.mesh_3.all_volumes
         for a_volume, vol_pres in zip(all_volumes, vols_press):
@@ -133,7 +134,7 @@ class PressureSolverTest(unittest.TestCase):
                     vol_pres[0], (4/3.0)*(1 - coord_x), delta=1e-15)
 
     def test_piece_wise_lin_prob_struct_with_dyn_point_at_adj_point(self):
-        imd = InterpolMethod(self.mesh_3, 1.0)
+        imd = InterpolMethods(self.mesh_3, 1.0)
         vols_press = self.mpfad_3.run_solver(imd.by_lpew2)
         all_volumes = self.mesh_3.all_volumes
         for a_volume, vol_pres in zip(all_volumes, vols_press):
@@ -146,7 +147,7 @@ class PressureSolverTest(unittest.TestCase):
                     vol_pres[0], (4/3.0)*(1 - coord_x), delta=1e-15)
 
     def test_piece_wise_lin_prob_unstruct_with_dyn_point_at_two_thirds(self):
-        imd = InterpolMethod(self.mesh_4, 2/3.0)
+        imd = InterpolMethods(self.mesh_4, 2/3.0)
         vols_press = self.mpfad_4.run_solver(imd.by_lpew2)
         all_volumes = self.mesh_4.all_volumes
         for a_volume, vol_pres in zip(all_volumes, vols_press):
@@ -159,7 +160,7 @@ class PressureSolverTest(unittest.TestCase):
                     vol_pres[0], (4/3.0)*(1 - coord_x), delta=1e-14)
 
     def test_piece_wise_lin_prob_unstruct_with_dyn_point_at_adj_point(self):
-        imd = InterpolMethod(self.mesh_4, 1.0)
+        imd = InterpolMethods(self.mesh_4, 1.0)
         vols_press = self.mpfad_4.run_solver(imd.by_lpew2)
         all_volumes = self.mesh_4.all_volumes
         for a_volume, vol_pres in zip(all_volumes, vols_press):
@@ -172,7 +173,7 @@ class PressureSolverTest(unittest.TestCase):
                     vol_pres[0], (4/3.0)*(1 - coord_x), delta=1e-13)
 
     def test_flux_conservative_dyn_point_at_adj_point(self):
-        imd = InterpolMethod(self.mesh_2, 1.0)
+        imd = InterpolMethods(self.mesh_2, 1.0)
         vols_press = self.mpfad_2.run_solver(imd.by_lpew2)
         A = self.mpfad_2.A
         B = self.mpfad_2.B
@@ -181,7 +182,7 @@ class PressureSolverTest(unittest.TestCase):
             self.assertAlmostEqual(residue[0], 0.0, delta=1e-15)
 
     def test_flux_conservative_dyn_point_at_two_thirds(self):
-        imd = InterpolMethod(self.mesh_2, 2.0/3.0)
+        imd = InterpolMethods(self.mesh_2, 2.0/3.0)
         vols_press = self.mpfad_2.run_solver(imd.by_lpew2)
         A = self.mpfad_2.A
         B = self.mpfad_2.B
